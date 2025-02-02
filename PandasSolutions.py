e0c91594b64e2f8db100ec104965a8784c668c52
adimishra555 <141378821+adimishra555@users.noreply.github.com>
2025-02-02 09:43:14 +0530

    PandasSolutions.py

--- PandasSolutions.py
+++ PandasSolutions.py
@@ -3,8 +3,15 @@
 
 -> Recyclable and Low Fat Products
 import pandas as pd
-
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     df = products[(products['low_fats'] =='Y') & (products['recyclable'] =='Y')]
     df = df[['product_id']]
     return df
+
+#  Find Customer Referee
+import pandas as pd
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    # res = customer[customer['referee_id'].fillna('')!=2]['name']
+    df = customer[customer['referee_id'].fillna(0) != 2]
+    res = df[['name']]
+    return res
