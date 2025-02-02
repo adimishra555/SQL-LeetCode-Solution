2a7a22a43e294d003192107065b79dead4edca57
adimishra555 <141378821+adimishra555@users.noreply.github.com>
2025-02-02 09:21:19 +0530

    PandasSolutions.py

--- PandasSolutions.py
+++ PandasSolutions.py
@@ -1,4 +1,10 @@
 ------------------------------------------------------------- PANDAS SOLUTION From LeetCode ----------------------------------------------------------------
 
 
-Recyclable and Low Fat Products
+-> Recyclable and Low Fat Products
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] =='Y') & (products['recyclable'] =='Y')]
+    df = df[['product_id']]
+    return df
